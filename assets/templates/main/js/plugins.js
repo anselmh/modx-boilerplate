590a066d46ef987dcf3306dced010a2877eea086
smooth-graphics <info@novolo.de>
2011-03-19 10:41:32 +0100

    1.2 (2011-03-19) + added accessifyhtml5.js to plugins.js ( https://github.com/yatil/accessifyhtml5.js ) [Issue: #13] + added CSS3PIE for IE-CSS3-properties [Issue: #5] - kill handheld.css (which already hadn't been referenced by me in <head> - oops) [Issue: #15] * improved setup docs (sorry folks, no real documentation before 1.5 as it needs a package for official RTFM in my eyes) [Issue: #12]
    Signed-off-by: smooth-graphics <info@novolo.de>

--- assets/templates/main/js/plugins.js
+++ assets/templates/main/js/plugins.js
@@ -9,3 +9,43 @@ window.log = function(){
 (function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
 
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
+
+/*
+ * Accessifyhtml5.js
+ * Adds ARIA to new elements in browsers which don't do it by themselves.
+ * Just drop into the bottom of your web page:
+ * <script src="accessifyhtml5.js"></script>
+ * 
+ * Yes, it depends on jQuery.
+ *
+ * Souce: http://www.html5accessibility.com/index-aria.html
+ *
+ * Todo: Extend Script for other elements, probably even play with fallback JS for inaccessible audio/video.
+ *
+ * Acknowledgements: 
+ * - @stevefaulkner for his work exploring html5 a11y,
+ * - @paddya91 for object notation & document ready
+ * - @ginader for reporting typo
+ * - @webaxe for reporting an error
+ */
+
+$(document).ready(function() {
+    
+    var fixes = {
+        'header.site'   : { 'role':          'banner'        },
+        'footer.site'   : { 'role':          'contentinfo'   },
+        'article'       : { 'role':          'article'       },
+        'aside'         : { 'role':          'complementary' },
+        'nav'           : { 'role':          'navigation'    },
+        'output'        : { 'aria-live':     'polite'        },
+        'section'       : { 'role':          'region'        },
+        '[required]'    : { 'aria-required': 'true'          }
+    };
+
+    $.each(fixes,
+        function(index, item) {
+            $(index).attr(item);
+        }
+    );
+    
+});

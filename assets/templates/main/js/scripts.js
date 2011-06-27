50ddf733de0f535bb65dbca4b6fd4ac35cdd3655
smooth-graphics <info@novolo.de>
2011-06-27 11:11:27 +0200

    * fixes #65: avoid user tracking for prerendering
    Signed-off-by: smooth-graphics <info@novolo.de>

--- assets/templates/main/js/scripts.js
+++ assets/templates/main/js/scripts.js
@@ -1,10 +1,29 @@
 // load all inline scripts here
 
-// Code for Google Analytics | Change UA-XXXXX-X to be your site's ID
-var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview'],['_trackPageLoadTime']];
-	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
-	g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-	s.parentNode.insertBefore(g,s)}(document,'script'));
+// if you have analytics, set them in pagevisibility mode to get real-user-trackig according to prerender-issues in Chrome/WebKit.
+var viewCounted = false;
+function handleVisibilityChange(evt) {
+  //Only count the view once we're done prerendering AND the document is done loading
+  if (document.webkitVisibilityState == 'prerender'
+      || document.readyState != 'complete' || viewCounted)
+    return;
+	/* Implementation of view counting system starts here */
+ 
+	// Code for Google Analytics | Change UA-XXXXX-X to be your site's ID
+	var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview'],['_trackPageLoadTime']];
+		(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+		g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+		s.parentNode.insertBefore(g,s)}(document,'script'));
+	
+	/* Implementation of view counting system ends here */
+	viewCounted = true;
+}
+//We'll call checkCountView on load no matter what.
+window.addEventListener('load', handleVisibilityChange, false);
+if (document.webkitVisibilityState == 'prerender') {
+  document.addEventListener('webkitvisibilitychange', handleVisibilityChange, false);
+}
+
 
 //Code for Facebook XBML implementation. Cut off if not needed. Please paste your own App-ID at 'AppID' to get it working
 window.fbAsyncInit = function() {

d7c327c0325fbe9e4f7ce15f87f9e1b4a75385c3
smooth-graphics <info@novolo.de>
2011-09-12 11:27:55 +0200

    jQuery free version of accessify.js
    Signed-off-by: smooth-graphics <info@novolo.de>

--- assets/templates/main/js/plugins.js
+++ assets/templates/main/js/plugins.js
@@ -3,25 +3,20 @@ window.log=function(){log.history=log.history||[];log.history.push(arguments);if
 {console.log();return window.console;}catch(err){return window.console={};}})());
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
 
-/*
- * Accessifyhtml5.js - adds ARIA to new elements in browsers which don't do it by themselves.
- * Depends on jQuery.
- * Source: http://www.html5accessibility.com/index-aria.html
-*/
-$(document).ready(function() {
-	var fixes = {
-        'header.site'   : { 'role':          'banner'        },
-        'footer.site'   : { 'role':          'contentinfo'   },
-        'article'       : { 'role':          'article'       },
-        'aside'         : { 'role':          'complementary' },
-        'nav'           : { 'role':          'navigation'    },
-        'output'        : { 'aria-live':     'polite'        },
-        'section'       : { 'role':          'region'        },
-        '[required]'    : { 'aria-required': 'true'          }
-    };
-    $.each(fixes,
-        function(index, item) {
-            $(index).attr(item);
-        }
-    );   
+
+var aria = [
+    'article | role | article',
+    'aside | role | complementary',
+    'nav | role | navigation',
+    'output | aria-live | polite',
+    'section | role | region',
+    '[required] | aria-required | true'
+];
+
+aria.forEach(function(value, index) {
+    var ariarow = value.split(' | ');
+    var elm = document.querySelector(ariarow[0])
+    if (elm) {
+        elm.setAttribute(ariarow[1], ariarow[2]);
+    }
 });

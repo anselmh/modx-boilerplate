5eb7708380f852520025d0e3aedbeb1ba4d4b977
smooth-graphics <info@novolo.de>
2011-09-12 11:33:45 +0200

    added new console api methods to console.log protection
    Signed-off-by: smooth-graphics <info@novolo.de>

--- assets/templates/main/js/plugins.js
+++ assets/templates/main/js/plugins.js
@@ -1,5 +1,5 @@
 window.log=function(){log.history=log.history||[];log.history.push(arguments);if(this.console){arguments.callee=arguments.callee.caller;var a=[].slice.call(arguments);(typeof console.log==="object"?log.apply.call(console.log,console,a):console.log.apply(console,a))}};
-(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,timeStamp,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c}})((function(){try
+(function(b){function c(){}for(var d="assert,clear,count,debug,dir,dirxml,error,exception,firebug,group,groupCollapsed,groupEnd,info,log,memoryProfile,memoryProfileEnd,profile,profileEnd,table,time,timeEnd,timeStamp,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c}})((function(){try
 {console.log();return window.console;}catch(err){return window.console={};}})());
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
 

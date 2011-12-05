4a0b10e08eeaaba7fcf7ff5f3de6269e7e8526ac
Anselm Hannemann <info@novolo.de>
2011-12-05 23:49:35 +0100

    update accessify and update GA snippet
    no important changes hère. dropped fixes #76 due to additional
    http-request

--- assets/templates/main/js/scripts.js
+++ assets/templates/main/js/scripts.js
@@ -1,11 +1,10 @@
 // load all inline scripts here
 
 // Code for Google Analytics | Change UA-XXXXX-X to be your site's ID
-	var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview'],['_trackPageLoadTime']];
-		(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
-		g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-		s.parentNode.insertBefore(g,s)}(document,'script'));
-
+var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview'],['_trackPageLoadTime']];
+	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+	g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+	s.parentNode.insertBefore(g,s)}(document,'script'));
 
 //Code for Facebook XBML implementation. Cut off if not needed. Please paste your own App-ID at 'AppID' to get it working
 window.fbAsyncInit = function() {

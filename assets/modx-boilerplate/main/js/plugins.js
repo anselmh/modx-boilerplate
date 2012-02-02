9c3a9d84d5f79e86bc965145c589931f34f22ec9
Anselm Hannemann <info@novolo.de>
2012-02-02 11:12:37 +0100

    update plugins.js's dev-tools script

--- assets/modx-boilerplate/main/js/plugins.js
+++ assets/modx-boilerplate/main/js/plugins.js
@@ -1,8 +1,10 @@
-// console log
-window.log=function(){log.history=log.history||[];log.history.push(arguments);if(this.console){arguments.callee=arguments.callee.caller;var a=[].slice.call(arguments);(typeof console.log==="object"?log.apply.call(console.log,console,a):console.log.apply(console,a))}};
-(function(b){function c(){}for(var d="assert,clear,count,debug,dir,dirxml,error,exception,firebug,group,groupCollapsed,groupEnd,info,log,memoryProfile,memoryProfileEnd,profile,profileEnd,table,time,timeEnd,timeStamp,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c}})((function(){try
-{console.log();return window.console;}catch(err){return window.console={};}})());
-// accessify.js from @yatil
-var AccessifyHTML5=function(g){if(document.querySelectorAll){var c={article:{role:"article"},aside:{role:"complementary"},nav:{role:"navigation"},output:{"aria-live":"polite"},section:{role:"region"},"[required]":{"aria-required":"true"}};if(g){if(g.header){c[g.header]={role:"banner"}}if(g.footer){c[g.footer]={role:"contentinfo"}}}for(fix in c){var b=document.querySelectorAll(fix),f=c[fix],a,e,d=0;for(key in f){a=key;e=f[key]}for(d;d<b.length;d++){b[d].setAttribute(a,e)}}}};
-//////////////////////////
+// usage: log('inside coolFunc', this, arguments);
+// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+window.log = function f(){ log.history = log.history || []; log.history.push(arguments); if(this.console) { var args = arguments, newarr; args.callee = args.callee.caller; newarr = [].slice.call(args); if (typeof console.log === 'object') log.apply.call(console.log, console, newarr); else console.log.apply(console, newarr);}};
+
+// make it safe to use console.log always
+(function(a){function b(){}for(var c="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),d;!!(d=c.pop());){a[d]=a[d]||b;}})
+(function(){try{console.log();return window.console;}catch(a){return (window.console={});}}());
+
+
 // place any jQuery/helper plugins in here, instead of separate, slower script files.

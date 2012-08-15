a9826ea95b4c73b968e1caeca7b562e04b6a7ba0
Anselm Hannemann <anselm@novolo.de>
2012-08-15 10:52:01 +0200

    Removed FBML
    This fixes #109

--- assets/modx-boilerplate/main/js/scripts.js
+++ assets/modx-boilerplate/main/js/scripts.js
@@ -1,13 +1,1 @@
 // load all inline scripts here
-
-//Code for Facebook XBML implementation. Cut off if not needed. Please paste your own App-ID at 'AppID' to get it working
-window.fbAsyncInit = function() {
-	FB.init({appId: 'AppID', status: true, cookie: true,
-	xfbml: true});
-};
-(function() {
-	var e = document.createElement('script'); e.async = true;
-	e.src = document.location.protocol +
-	'//connect.facebook.net/en_US/all.js';
-	document.getElementById('fb-root').appendChild(e);
-}());
